22117aadf35b869d5947c08ff236007c7385e4ca
S Dheeraj <97402083+SaranaDheeraj@users.noreply.github.com>
2024-05-18 16:05:12 +0530

    Update gpt.js

--- src/gpt.js
+++ src/gpt.js
@@ -3,7 +3,7 @@ import { Configuration, OpenAIApi } from "openai";
 import { jsonrepair } from "jsonrepair";
 
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
